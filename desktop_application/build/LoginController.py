0f01189bd4d59070b952215ee2f6e69f0f0cba09
FengTing Guo <guofengtingalt@gmail.com>
2025-04-29 15:27:28 -0400

    working on desktop ui

---
+++ desktop_application/build/LoginController.py
@@ -0,0 +1,18 @@
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+    print("hello")
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+
+pokemon_name = "pikachu"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+
