f228f741ce9c04e9ddf7685d107a8f332e0d2e9e
FengTing Guo <guofengtingalt@gmail.com>
2025-05-05 10:18:14 -0400

    update gitignore

---
+++ acla_backend/dist/user-info/dto/create-user.dto.d.ts
@@ -0,0 +1,3 @@
+export declare class CreateUserInfoDto {
+    name: string;
+}
---
+++ acla_backend/dist/user-info/dto/user-info.model.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userInfoDto = void 0;
+class userInfoDto {
+    id;
+}
+exports.userInfoDto = userInfoDto;
+//# sourceMappingURL=user-info.model.js.map
