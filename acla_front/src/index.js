eaffa8016926e09445a8b9950a759d293b483712
FengTing Guo <guofengting98@gmail.com>
2025-05-27 10:25:49 -0400

    working on login

--- acla_front/src/index.js
+++ acla_front/src/index.js
@@ -5,7 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+
+//The purpose of the function is to define the HTML element where a React component should be displayed.
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//define the React component that should be rendered.
+//  In "public" folder, there is an index.html file. This is where our React application will be rendered.
+//Display inside an element with the id of "root":
 root.render(
   <React.StrictMode>
     <Theme>
