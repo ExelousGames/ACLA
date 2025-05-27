ac7965354873e718fdcbce8366b72fcbb3fcd1e0
FengTing Guo <guofengting98@gmail.com>
2025-05-26 23:14:56 -0400

    working on login

--- acla_front/src/create-user-info.js
+++ acla_front/src/create-user-info.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './App.css';
+import './create-user-info.css';
+
 import {
 	Avatar,
 	Badge,
@@ -38,54 +41,56 @@ const CreateUserInfo = ({ onTaskCreated }) => {
     };
 
     return (
-        <Box maxWidth="350px">
-            <Card size="4">
-                        <Heading as="h3" size="6" trim="start" mb="5">
-                            Login
-                        </Heading>
+        <div className = 'center'>
+            <Box width="400px" height="64px">
+                <Card size="4">
+                            <Heading as="h3" size="6" trim="start" mb="5">
+                                Login
+                            </Heading>
 
-                        <Box mb="5">
-                            <Flex mb="1">
-                                <Text
-                                    as="label"
-                                    htmlFor="example-email-field" 
-                                    size="2"
-                                    weight="bold"
-                                >
-                                    Email address
-                                </Text>
+                            <Box mb="5">
+                                <Flex mb="1">
+                                    <Text
+                                        as="label"
+                                        htmlFor="example-email-field" 
+                                        size="2"
+                                        weight="bold"
+                                    >
+                                        Email address
+                                    </Text>
+                                </Flex>
+                                <TextField.Root
+                                    placeholder="Enter your email"
+                                    id="example-email-field"
+                                />
+                            </Box>
+
+                            <Box mb="5" position="relative">
+                                <Flex align="baseline" justify="between" mb="1">
+                                    <Text
+                                        as="label"
+                                        size="2"
+                                        weight="bold"
+                                        htmlFor="example-password-field"
+                                    >
+                                        Password
+                                    </Text>
+                                </Flex>
+                                <TextField.Root
+                                    placeholder="Enter your password"
+                                    id="example-password-field"
+                                />
+                            </Box>
+
+                            <Flex mt="6" justify="end" gap="3">
+                                <Button variant="outline">
+                                    Create an account
+                                </Button>
+                                <Button>Sign in</Button>
                             </Flex>
-                            <TextField.Root
-                                placeholder="Enter your email"
-                                id="example-email-field"
-                            />
-                        </Box>
-
-                        <Box mb="5" position="relative">
-                            <Flex align="baseline" justify="between" mb="1">
-                                <Text
-                                    as="label"
-                                    size="2"
-                                    weight="bold"
-                                    htmlFor="example-password-field"
-                                >
-                                    Password
-                                </Text>
-                            </Flex>
-                            <TextField.Root
-                                placeholder="Enter your password"
-                                id="example-password-field"
-                            />
-                        </Box>
-
-                        <Flex mt="6" justify="end" gap="3">
-                            <Button variant="outline">
-                                Create an account
-                            </Button>
-                            <Button>Sign in</Button>
-                        </Flex>
-            </Card>
-        </Box>
+                </Card>
+            </Box>
+        </div>
     );
 };
 
