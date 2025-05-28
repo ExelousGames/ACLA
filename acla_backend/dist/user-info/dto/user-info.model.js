6cf99969f806e211af1de3307438bf625488c630
FengTing Guo <guofengting98@gmail.com>
2025-05-28 13:21:09 -0400

    delete build files

--- acla_backend/dist/user-info/dto/user-info.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userInfoDto = void 0;
-class userInfoDto {
-    id;
-}
-exports.userInfoDto = userInfoDto;
-//# sourceMappingURL=user-info.model.js.map
