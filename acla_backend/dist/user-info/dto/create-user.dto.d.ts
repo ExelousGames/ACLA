bba5b3d7684d9dcbd31984c540ef95e67d49f3cd
FengTing Guo <guofengting98@gmail.com>
2025-05-28 13:21:09 -0400

    delete build files

--- acla_backend/dist/user-info/dto/create-user.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class CreateUserInfoDto {
-    name: string;
-}
--- acla_backend/dist/user-info/dto/create-user.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateUserInfoDto = void 0;
-class CreateUserInfoDto {
-    name;
-}
-exports.CreateUserInfoDto = CreateUserInfoDto;
-//# sourceMappingURL=create-user.dto.js.map
