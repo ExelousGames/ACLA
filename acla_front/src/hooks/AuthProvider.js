67e32d1c93498ebd6055c2f18d84fd266e0c4516
FengTing Guo <guofengting98@gmail.com>
2025-05-29 13:06:00 -0400

    Update AuthProvider.js

--- acla_front/src/hooks/AuthProvider.js
+++ acla_front/src/hooks/AuthProvider.js
@@ -18,11 +18,11 @@ const AuthProvider = ({ children }) => {
     //backend server api
     const serverIPandPort = process.env.REACT_APP_BACKEND_SERVER_IP + ":" + process.env.REACT_APP_BACKEND_PROXY_PORT
     const server_url_header = 'http://' + serverIPandPort
-    alert("server_url_header" + '/userinfo/auth/login');
+    alert(server_url_header);
     //handles user login by sending a POST request to an authentication endpoint, 
     // updating the user and token state upon a successful response, and storing the token in local storage.
     const loginAction = async (data) => {
-        
+        alert(server_url_header + '/userinfo/auth/login');
         axios.post(server_url_header + '/userinfo/auth/login', data)
             .then(response => {
                
