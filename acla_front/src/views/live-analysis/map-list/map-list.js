aa6fa63bfb5d52ef8c098f0dd1bd526c30c6a201
FengTing Guo <guofengting98@gmail.com>
2025-06-03 09:11:51 -0400

    working on ui

--- acla_front/src/views/live-analysis/map-list/map-list.js
+++ acla_front/src/views/live-analysis/map-list/map-list.js
@@ -4,6 +4,7 @@ import './map-list.css';
 import {
     AspectRatio,
     Badge,
+    Avatar,
     Box,
     Button,
     Card,
@@ -21,31 +22,61 @@ import {
     TextField,
     Theme,
 } from "@radix-ui/themes";
-import { ScrollArea, color, Inset, } from "radix-ui";
+import { ScrollArea } from "radix-ui";
 const MapContext = createContext();
 
-const MapList = () => {
-    const albumsFavorites = []
+const MapList = (setMapState) => {
+    const albumsFavorites = [1, 2, 3, 4, 5]
+
     return (
-        <ScrollArea type="always" scrollbars="vertical" style={{ height: 180 }}>
 
-            <Grid row="5" gap="5" mb="7">
-                {albumsFavorites.map((album) => (
-                    <MapContext.Provider>
-                        <MapCard />
-                    </MapContext.Provider>
-                ))}
-            </Grid>
+        <ScrollArea.Root className="MapListScrollAreaRoot">
+            <ScrollArea.Viewport className="ScrollAreaViewport">
+                <Grid row="5" gap="5" mb="7">
+                    {albumsFavorites.map((album) => (
+                        <MapContext.Provider>
+                            <MapCard />
+                        </MapContext.Provider>
+                    ))}
+                </Grid>
+            </ScrollArea.Viewport>
+            <ScrollArea.Scrollbar
+                className="ScrollAreaScrollbar"
+                orientation="vertical"
+            >
+                <ScrollArea.Thumb className="ScrollAreaThumb" />
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Scrollbar
+                className="ScrollAreaScrollbar"
+                orientation="horizontal"
+            >
+                <ScrollArea.Thumb className="ScrollAreaThumb" />
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Corner className="ScrollAreaCorner" />
+        </ScrollArea.Root>
 
-        </ScrollArea>
+
     )
 };
 
-const MapCard = () => {
+const MapCard = (setMapState) => {
     const theMap = useContext(MapContext);
     return (
-        <Box p="3" m="-3">
-        </Box>
+
+        <Button >
+            <Flex gap="3" align="center">
+                <Avatar variant="solid" fallback="A" />
+                <Box>
+                    <Text as="div" size="2" weight="bold">
+                        Teodros Girmay
+                    </Text>
+                    <Text as="div" size="2" color="gray">
+                        Engineering
+                    </Text>
+                </Box>
+            </Flex>
+        </Button>
+
 
     )
 };
