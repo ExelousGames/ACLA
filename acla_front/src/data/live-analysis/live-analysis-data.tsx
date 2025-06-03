cac39d4265067d6f1e423173d7c176f2348ee9ad
FengTing Guo <guofengting98@gmail.com>
2025-06-03 16:30:22 -0400

    added map, session list, session tabs

---
+++ acla_front/src/data/live-analysis/live-analysis-data.tsx
@@ -0,0 +1,11 @@
+export type MapOption = {
+    key: number,
+    name: string;
+    session_count: number;
+}
+
+export type SessionOption = {
+    key: number,
+    name: string;
+    total_time: number;
+}
