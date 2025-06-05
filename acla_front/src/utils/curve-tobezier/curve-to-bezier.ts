ccd8f30e2caed91893ddc8a4c42ef26c6506e596
FengTing Guo <guofengting98@gmail.com>
2025-06-04 23:08:50 -0400

    working on map

--- acla_front/src/utils/curve-tobezier/curve-to-bezier.ts
+++ acla_front/src/utils/curve-tobezier/curve-to-bezier.ts
@@ -1,7 +1,7 @@
 import { Point } from './points-on-curve.js';
 
 function clone(p: Point): Point {
-  return [...p] as Point;
+  return { ...p };
 }
 
 /**
@@ -37,10 +37,10 @@ export function curveToBezier(pointsIn: readonly Point[], curveTightness = 0): P
     out.push(clone(points[0]));
     for (let i = 1; (i + 2) < points.length; i++) {
       const cachedVertArray = points[i];
-      b[0] = [cachedVertArray[0], cachedVertArray[1]];
-      b[1] = [cachedVertArray[0] + (s * points[i + 1][0] - s * points[i - 1][0]) / 6, cachedVertArray[1] + (s * points[i + 1][1] - s * points[i - 1][1]) / 6];
-      b[2] = [points[i + 1][0] + (s * points[i][0] - s * points[i + 2][0]) / 6, points[i + 1][1] + (s * points[i][1] - s * points[i + 2][1]) / 6];
-      b[3] = [points[i + 1][0], points[i + 1][1]];
+      b[0] = { x: cachedVertArray.x, y: cachedVertArray.y };
+      b[1] = { x: cachedVertArray.x + (s * points[i + 1].x - s * points[i - 1].x) / 6, y: cachedVertArray.y + (s * points[i + 1].y - s * points[i - 1].y) / 6 };
+      b[2] = { x: points[i + 1].x + (s * points[i].x - s * points[i + 2].x) / 6, y: points[i + 1].y + (s * points[i].y - s * points[i + 2].y) / 6 };
+      b[3] = { x: points[i + 1].x, y: points[i + 1].y };
       out.push(b[1], b[2], b[3]);
     }
   }
--- acla_front/src/views/live-analysis/sessionAnalysis/sessionAnalysis.tsx
+++ acla_front/src/views/live-analysis/sessionAnalysis/sessionAnalysis.tsx
@@ -1,21 +1,22 @@
-import { createContext, useContext, useEffect, useRef, useState } from 'react';
-import { Stage, Layer, Arc, Circle, Rect } from 'react-konva';
+import { createContext, Key, useContext, useEffect, useRef, useState } from 'react';
+import { Stage, Layer, Arc, Circle, Rect, Line, Group } from 'react-konva';
+import { Point } from 'utils/curve-tobezier/points-on-curve';
 
 const SessionAnalysis = () => {
 
     // Define virtual size for our scene
-    let containerWidth = 500;
-    let containerHeight = 500;
-
+    let containerWidth = window.innerWidth;
+    let containerHeight = window.innerWidth;
     // State to track current scale and dimensions
     const [stageSize, setStageSize] = useState({
         width: containerWidth,
         height: containerHeight,
-        scale: 1
     });
-
+    const [shapes, setShapes] = useState(createInitialShapes());
     // Reference to parent container
     const containerRef = useRef<HTMLInputElement>(null);
+
+    ///////////////functions////////////////////
 
     // Function to handle resize
     const updateSize = () => {
@@ -24,52 +25,102 @@ const SessionAnalysis = () => {
         // Get container width
         containerWidth = containerRef.current.offsetWidth;
         containerHeight = containerRef.current.offsetHeight;
-
+        console.log(containerWidth, containerHeight);
         // Update state with new dimensions
         setStageSize({
             width: containerWidth,
-            height: containerWidth,
-            scale: 1
+            height: containerHeight,
         });
     };
 
     // Update on mount and when window resizes
     useEffect(() => {
-
         updateSize();
-
-
     }, []);
 
+    function handleDragMove(e: any, id: any) {
+        const target = e.target;
+        const targetRect = target.getClientRect();
+
+        setShapes(shapes.map((shape: { id: any; }) => {
+            if (shape.id === id) {
+                return shape;
+            }
+            const shapeGroup = target.parent.parent.findOne(`#group-${shape.id}`);
+            if (!shapeGroup) return shape;
+        }));
+
+        setShapes(shapes.map((shape: { id: any; }) => {
+            if (shape.id !== id) return;
+
+            let point: any[] = [e.target.x(), e.target.y()];
+
+            console.log(e.target.x(), e.target.y());
+            if (e.target.x() <= 0) {
+                point[0] = 0;
+            }
+            if (e.target.x() as number >= stageSize.width) {
+                point[0] = stageSize.width;
+            }
+
+            if (e.target.y() < 0) {
+                point[1] = 0;
+            }
+            if (e.target.y() as number >= stageSize.height) {
+                point[1] = stageSize.height;
+            }
+
+            //set posisition
+            e.target.absolutePosition({
+                x: point[0],
+                y: point[1]
+            });
+
+            //record position
+            if (shape.id === id) {
+                return { ...shape, x: point[0], y: point[1] }
+            }
+            return shape;
+        }
+
+        ));
+    }
+
+    const handleDragEnd = (e: any, id: any) => {
+
+    };
+
+    function createInitialShapes(): any {
+        return [
+            { id: 0, x: 0, y: 0 }
+        ]
+    }
     return (
         <div ref={containerRef} style={{ width: '100%', height: '90%' }}>
 
-            <Stage width={stageSize.width}
-                height={stageSize.height}
-                scaleX={stageSize.scale}
-                scaleY={stageSize.scale}>
-
+            <Stage width={stageSize.width} height={stageSize.height} >
                 <Layer>
-                    <Circle
-                        radius={50}
-                        fill="red"
-                        x={containerWidth / 2}
-                        y={containerHeight / 2}
-                    />
-                    <Rect
-                        fill="green"
-                        x={containerWidth - 100}
-                        y={containerHeight - 100}
-                        width={100}
-                        height={100}
-                    />
+                    {shapes.map((shape: { id: Key; x: number | undefined; y: number | undefined; }) => (
+                        <Group key={shape.id} id={`group-${shape.id}`} x={shape.x} y={shape.y} draggable onDragMove={(e) => handleDragMove(e, shape.id)} onDragEnd={(e) => handleDragEnd(e, shape.id)}>
+                            <Circle key={shape.id} radius={20} fill={"red"} name={shape.id.toString()} />
+                        </Group>
+                    ))}
                 </Layer>
             </Stage>
         </div>
     );
 };
 
+function conver_1D_array_to_2d_array(points: number[]): Point[] {
+    const result: Point[] = [];
+    for (let i = 0; i < points.length; i += 2) {
+        const point = points.slice(i, i + 2);
+        result.push({ x: point[0], y: point[1] });
+    }
+    return result;
+}
 
 
 export default SessionAnalysis;
+
 
