ba6e1061bcda1b950b9b48c4d5ff91fa96ebcda0
FengTing Guo <guofengting98@gmail.com>
2025-06-10 13:58:06 -0400

    Update header-menu.tsx

--- acla_front/src/views/header-menu/header-menu.tsx
+++ acla_front/src/views/header-menu/header-menu.tsx
@@ -35,33 +35,19 @@ const HeaderMenu = () => {
             <DropdownMenu.Root>
                 <DropdownMenu.Trigger>
                     <Button variant="soft">
-                        Options
+                        <Avatar
+                            src=""
+                            fallback="A"
+                        />
+                        <Avatar fallback="A" />
                         <DropdownMenu.TriggerIcon />
                     </Button>
                 </DropdownMenu.Trigger>
                 <DropdownMenu.Content>
-                    <DropdownMenu.Item shortcut="⌘ E">Edit</DropdownMenu.Item>
-                    <DropdownMenu.Item shortcut="⌘ D">Duplicate</DropdownMenu.Item>
-                    <DropdownMenu.Separator />
-                    <DropdownMenu.Item shortcut="⌘ N">Archive</DropdownMenu.Item>
-
-                    <DropdownMenu.Sub>
-                        <DropdownMenu.SubTrigger>More</DropdownMenu.SubTrigger>
-                        <DropdownMenu.SubContent>
-                            <DropdownMenu.Item>Move to project…</DropdownMenu.Item>
-                            <DropdownMenu.Item>Move to folder…</DropdownMenu.Item>
-
-                            <DropdownMenu.Separator />
-                            <DropdownMenu.Item>Advanced options…</DropdownMenu.Item>
-                        </DropdownMenu.SubContent>
-                    </DropdownMenu.Sub>
-
-                    <DropdownMenu.Separator />
-                    <DropdownMenu.Item>Share</DropdownMenu.Item>
-                    <DropdownMenu.Item>Add to favorites</DropdownMenu.Item>
+                    <DropdownMenu.Item shortcut="⌘ E">Profile</DropdownMenu.Item>
                     <DropdownMenu.Separator />
                     <DropdownMenu.Item shortcut="⌘ ⌫" color="red">
-                        Delete
+                        Log out
                     </DropdownMenu.Item>
                 </DropdownMenu.Content>
             </DropdownMenu.Root>
