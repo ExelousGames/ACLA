61fb49af27aedee024cbeaf31d13a1385af856e6
FengTing Guo <guofengting98@gmail.com>
2025-06-13 10:07:49 -0400

    updates

--- acla_front/src/data/live-analysis/live-analysis-data.tsx
+++ acla_front/src/data/live-analysis/live-analysis-data.tsx
@@ -1,15 +1,13 @@
 export type MapOption = {
     //key is required for child component of MapList component
-    key?: number,
-    datakey: number,
+    key: number,
     name?: string;
     session_count?: number;
 }
 
 export type SessionOption = {
     //key is required for child component of MapList component
-    key?: number,
-    datakey: number,
+    key: number,
     name: string;
     total_time: number;
 }
